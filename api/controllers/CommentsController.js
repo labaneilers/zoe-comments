bf9e7d5630db36731c71987fd4a2ff22f6cf80b4
Laban Eilers <laban@eilers.com>
2017-02-11 14:40:45 -0500

    Mongo support

--- api/controllers/CommentsController.js
+++ api/controllers/CommentsController.js
@@ -9,12 +9,24 @@ var db = require("../services/db").db;
 
 module.exports = {
 	list: function (req, res) {
+        console.log("called");
         var newComment = req.param("comment");
         if (newComment) {
-            db.addComment(newComment);
+            db.addComment(newComment, function(err) {
+                if (err) {
+                    console.log("ERROR inserting: " + err)
+                }
+            });
         }
 
-        return res.view("commentslist", { comments: db.listComments() });
+        db.listComments(function(err, comments) {
+            console.log("got comments list");
+            if (err) {
+                console.log("ERROR listing: " + err);
+            }
+
+            return res.view("commentslist", { comments: comments });
+        });
     },
 };
 
--- api/services/db.js
+++ api/services/db.js
@@ -1,18 +1,40 @@
-if (!appglobal.db) {
-    appglobal.db = {
-        tables: {
-            comments: []
+var MongoClient = require('mongodb').MongoClient;
+
+// Connect to the db
+function addComment(text, callback) {
+    commentsCollection(function(err, comments) {
+        if (err) {
+            callback(err);
+            return;
         }
-    };
+        comments.insert({ text: text }, { string: true }, callback);
+    });
+}
+
+function listComments(callback) {
+    commentsCollection(function(err, comments) {
+        if (err) {
+            callback(err);
+            return;
+        }
+        comments.find().toArray(callback);
+    });
+}
+
+function commentsCollection(callback) {
+    MongoClient.connect("mongodb://localhost:27017/zoescomments", function(err, db) {
+        if (err) {
+            callback(err);
+            return;
+        }
+
+        callback(null, db.collection('comments'));
+    });
 }
 
 var db = {
-    addComment: function(comment) {
-        appglobal.db.tables.comments.push(comment);
-    },
-    listComments: function() {
-        return appglobal.db.tables.comments;
-    }
+    addComment: addComment,
+    listComments: listComments
 }
 
 exports.db = db;
